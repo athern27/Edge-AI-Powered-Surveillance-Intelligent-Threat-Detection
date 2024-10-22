1e3072ffb356bc4e92dd46b9424357e43661a7d6
Kartik Khandelwal <98205378+athern27@users.noreply.github.com>
2024-10-22 15:17:54 +0530

    Update and rename code/TACT-Node/network_esp32.py to code/TACT-Node/SD-card/network_esp32.py

--- code/TACT-Node/SD-card/network_esp32.py
+++ code/TACT-Node/SD-card/network_esp32.py
@@ -1,10 +1,3 @@
-# This file is part of MaixPY
-# Copyright (c) sipeed.com
-#
-# Licensed under the MIT license:
-#   http://www.opensource.org/licenses/mit-license.php
-#
-
 import time, network
 from Maix import GPIO
 from fpioa_manager import fm
